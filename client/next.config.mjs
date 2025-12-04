d108b0db61d6f00c7b77dd172f97372fdce9ff5f
nithin-daniel <nithindaniel2018@gmail.com>
2025-12-04 15:56:53 +0530

    update: remove standalone output setting and delete vercel.json configuration

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
   images: { unoptimized: true },
   typescript: { ignoreBuildErrors: true },
-  experimental: { serverActions: { allowedOrigins: ["*"] } },
 };
 
 export default nextConfig;
